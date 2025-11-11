f2c22bb9e8a7ac5372cdcddda40eb65540ea45ec
Kuczok2 <139169055+Kuczok2@users.noreply.github.com>
2025-11-11 14:38:51 +0100

    Delete wp-content directory

--- wp-content/plugins/modula-best-grid-gallery/assets/js/admin/gallery-listing-ai/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e1624e6da2ac3c116680');
--- wp-content/plugins/simply-static/src/admin/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '75af936b575d859e75a0');
